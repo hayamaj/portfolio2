7a8923de5fb9b4d3117b6411992e172143ed3be3
hayamaj <hayamaj@Hayas-MacBook-Air.local>
2024-06-16 15:15:18 -0400

    button

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -6,7 +6,7 @@ const MagicButton = ({title, icon, position, handleClick, otherClasses}:{title:s
         // Button code
         <a
         href="/AlMajali-Resume.pdf"
-        download="AlMajali-Resume.pdf" className="sm:px-8 sm:py-4 px-4 py-2 text-xs rounded-full bg-[#0D0628] font-bold text-[#E9C6DD] tracking-widest uppercase sm:text-sm transform hover:scale-105 hover:bg-[#333DEF] transition-colors duration-200">
+        download="AlMajali-Resume.pdf" className="sm:px-8 sm:py-4 px-4 py-2 text-xs rounded-full bg-[#FAF9F6] font-bold text-[#E9C6DD] tracking-widest uppercase sm:text-sm transform hover:scale-105 hover:bg-[#333DEF] transition-colors duration-200">
   download my resume
 </a>
       
